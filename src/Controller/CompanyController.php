8f5cf5c81e3ce792284c97cde33e6ed0532f2838
leoizana <leo.germain2005@gmail.com>
2024-09-12 12:55:25 +0200

    Enregistrement d'une entreprise

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/company')]
 class CompanyController extends AbstractController
 {
-    #[Route('/company', name: 'app_company')]
+    #[Route('/', name: 'company_index')]
     public function index(): Response
     {
         return $this->render('company/index.html.twig', [
             'controller_name' => 'CompanyController',
         ]);
+    }
+    #[Route('/add1', name: 'company_add1')]
+    public function addFirst(): Response
+    {
+        return new Response("Entreprise 'STURNIO' créee avec succès !")
+        ;
     }
 }
