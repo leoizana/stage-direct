09d3afe3358d329e1b49985b6d233ad59c56354a
leoizana <leo.germain2005@gmail.com>
2024-09-16 11:29:37 +0200

    Création et affichage des entreprises

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,23 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CompanyRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Company;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/company')]
 class CompanyController extends AbstractController
 {
     #[Route('/', name: 'company_index')]
-    public function index(): Response
+    public function index(CompanyRepository $companyRepository, Request $request): Response
     {
+        $companies = $companyRepository->findAll();
+        dump($request);
+
+
+
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+            'companies' => $companies,
         ]);
     }
     #[Route('/add1', name: 'company_add1')]
-    public function addFirst(): Response
+    public function addFirst(EntityManagerInterface $entityManager): Response
     {
+        $sturno = new Company();
+        $sturno->setName("STURNO");
+        // $entityManager = $this-getDoctrine()->getManager();
+        $entityManager->persist($sturno);
+        $entityManager->flush();
         return new Response("Entreprise 'STURNIO' créee avec succès !")
         ;
     }
