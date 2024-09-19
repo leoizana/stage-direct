d3ee27c230e8ff9ea06a199dc8f60fa94a4d4f4c
leoizana <leo.germain2005@gmail.com>
2024-09-19 12:12:12 +0200

    Ajout d'un formulaire pour créer une entreprise

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -1,22 +1,36 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\CompanyType;
+use App\Entity\Company;
 use App\Repository\CompanyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Company;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/company')]
 class CompanyController extends AbstractController
-{
+{   
+    /* PHP version < 8
+    private $companyRepository;
+    public function __construct(CompanyRepository $companyRepository){
+        $this->companyRepository = $companyRepository;
+    }
+        */
+    // PHP version >= 8
+    public function __construct(
+        private CompanyRepository $companyRepository, 
+        private FormFactoryInterface $formFactory, 
+        private  EntityManagerInterface $entityManager){
+    }
+
     #[Route('/', name: 'company_index')]
-    public function index(CompanyRepository $companyRepository, Request $request): Response
+    public function index( Request $request): Response
     {
-        $companies = $companyRepository->findAll();
+        $companies = $this->companyRepository->findAll();
         dump($request);
 
 
@@ -26,43 +40,47 @@ class CompanyController extends AbstractController
         ]);
     }
 
-    #[Route('/add', name: 'company_add')]
-    public function add(EntityManagerInterface $entityManager): Response
-    {
-        $sturno = new Company();
-        $sturno->setName("STURNO");
-        // $entityManager = $this-getDoctrine()->getManager();
-        $entityManager->persist($sturno);
-        $entityManager->flush();
-        return new Response("Entreprise 'STURNIO' créee avec succès !")
-        ;
-    }
-
     #[Route('/update/{id}', name: 'company_update')]
-    public function modificationCompany(int $id,EntityManagerInterface $entityManager, CompanyRepository $companyRepository): Response
+    public function modificationCompany(int $id): Response
     {
-        $company = $companyRepository->find($id);
+        $company = $this->companyRepository->find($id);
         $company->setName("TC Bois");
 
         //$entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($company);
+        $this->entityManager->persist($company);
 
-        $entityManager ->flush();
+        $this->entityManager ->flush();
 
         return new Response("Entreprise modifier avec succès !")
         ;
     }
     
     #[Route('/delete/{id}', name: 'company_delete')]
-    public function deleteCompany(int $id,EntityManagerInterface $entityManager, CompanyRepository $companyRepository): Response
+    public function deleteCompany(int $id,): Response
     {
-        $company = $companyRepository->find($id);
+        $company = $this->companyRepository->find($id);
 
         //$entityManager = $this->getDoctrine()->getManager();
-        $entityManager->remove($company);
-        $entityManager ->flush();
+        $this->entityManager->remove($company);
+        $this->entityManager ->flush();
 
         return new Response("Entreprise supprimer avec succès !")
         ;
+    }
+    #[Route('/add', name: 'company_add')]
+    public function add(Request $request,): Response
+    {
+        $company = new Company();
+        $form = $this->formFactory->create(CompanyType::class, $company);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($company);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('company_index');
+        }
+
+        return $this->render('company/create.html.twig', ['form' => $form->createView()]);
     }
 }
