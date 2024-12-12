7877d127c10a77bcd667226a8c4f25a79b484618
leoizana <leo.germain2005@gmail.com>
2024-12-12 12:34:34 +0100

    Modification du css sur certaines pages, accès au bouton supprimer et refonte de école

--- src/Controller/SchoolController.php
+++ src/Controller/SchoolController.php
@@ -24,59 +24,59 @@ final class SchoolController extends AbstractController
     }
 
     #[Route('/new', name: 'app_school_new', methods: ['GET', 'POST'])]
-public function new(Request $request, EntityManagerInterface $entityManager): Response
-{
-    // Création d'une nouvelle instance de l'entité School
-    $school = new School();
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Création d'une nouvelle instance de l'entité School
+        $school = new School();
 
-    // Création du formulaire en liant le formulaire à l'entité School
-    $form = $this->createForm(SchoolType::class, $school);
+        // Création du formulaire en liant le formulaire à l'entité School
+        $form = $this->createForm(SchoolType::class, $school);
 
-    // Traitement de la requête pour vérifier si le formulaire a été soumis
-    $form->handleRequest($request);
+        // Traitement de la requête pour vérifier si le formulaire a été soumis
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        // Récupération des nouvelles classes ajoutées dynamiquement
-        $newGrades = $form->get('newGrade')->getData();
-        dump($newGrades);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération des nouvelles classes ajoutées dynamiquement
+            $newGrades = $form->get('newGrade')->getData();
+            dump($newGrades);
 
-        // Vérification et ajout des nouvelles classes
-        if (is_array($newGrades)) {
-            foreach ($newGrades as $newGradeName) {
-                if ($newGradeName) {
-                    $existingGrade = $entityManager->getRepository(Grade::class)->findOneBy(['className' => $newGradeName]);
+            // Vérification et ajout des nouvelles classes
+            if (is_array($newGrades)) {
+                foreach ($newGrades as $newGradeName) {
+                    if ($newGradeName) {
+                        $existingGrade = $entityManager->getRepository(Grade::class)->findOneBy(['className' => $newGradeName]);
 
-                    // Si la classe n'existe pas, on la crée
-                    if (!$existingGrade) {
-                        $newGrade = new Grade();
-                        $newGrade->setClassName($newGradeName);
-                        $entityManager->persist($newGrade);
-                        $school->addGrade($newGrade);
-                    } else {
-                        // Sinon, on associe la classe existante à l'école
-                        $school->addGrade($existingGrade);
+                        // Si la classe n'existe pas, on la crée
+                        if (!$existingGrade) {
+                            $newGrade = new Grade();
+                            $newGrade->setClassName($newGradeName);
+                            $entityManager->persist($newGrade);
+                            $school->addGrade($newGrade);
+                        } else {
+                            // Sinon, on associe la classe existante à l'école
+                            $school->addGrade($existingGrade);
+                        }
                     }
                 }
             }
+
+            // Enregistrement de l'école et des relations
+            $entityManager->persist($school);
+            $entityManager->flush();
+
+            // Redirection après soumission réussie
+            return $this->redirectToRoute('app_school_index');
         }
 
-        // Enregistrement de l'école et des relations
-        $entityManager->persist($school);
-        $entityManager->flush();
-
-        // Redirection après soumission réussie
-     return $this->redirectToRoute('app_school_index');
+        // Si le formulaire n'est pas encore soumis ou invalide, on le passe à la vue
+        return $this->render('school/new.html.twig', [
+            'form' => $form->createView(), // Passe la vue du formulaire à Twig
+        ]);
     }
 
-    // Si le formulaire n'est pas encore soumis ou invalide, on le passe à la vue
-    return $this->render('school/new.html.twig', [
-        'form' => $form->createView(), // Passe la vue du formulaire à Twig
-    ]);
-}
 
-    
-    
-    
+
+
 
     #[Route('/{id}', name: 'app_school_show', methods: ['GET'])]
     public function show(School $school): Response
@@ -91,24 +91,50 @@ public function new(Request $request, EntityManagerInterface $entityManager): Re
     {
         $form = $this->createForm(SchoolType::class, $school);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
-            // After editing, flush changes to the database
+            // Récupération des nouvelles classes ajoutées dynamiquement
+            $newGrades = $form->get('newGrade')->getData();
+            dump($newGrades);
+    
+            // Vérification et ajout des nouvelles classes
+            if (is_array($newGrades)) {
+                foreach ($newGrades as $newGradeName) {
+                    if ($newGradeName) {
+                        $existingGrade = $entityManager->getRepository(Grade::class)->findOneBy(['className' => $newGradeName]);
+    
+                        // Si la classe n'existe pas, on la crée
+                        if (!$existingGrade) {
+                            $newGrade = new Grade();
+                            $newGrade->setClassName($newGradeName);
+                            $entityManager->persist($newGrade);
+                            $school->addGrade($newGrade);
+                        } else {
+                            // Sinon, on associe la classe existante à l'école
+                            if (!$school->getGrades()->contains($existingGrade)) {
+                                $school->addGrade($existingGrade);
+                            }
+                        }
+                    }
+                }
+            }
+    
+            // Enregistrement des modifications
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_school_index', [], Response::HTTP_SEE_OTHER);
+    
+            return $this->redirectToRoute('app_school_index');
         }
-
+    
         return $this->render('school/edit.html.twig', [
             'school' => $school,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/{id}', name: 'app_school_delete', methods: ['POST'])]
     public function delete(Request $request, School $school, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$school->getId(), $request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $school->getId(), $request->get('_token'))) {
             $entityManager->remove($school);
             $entityManager->flush();
         }
